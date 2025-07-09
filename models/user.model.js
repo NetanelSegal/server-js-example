85ca1a869a638d329a350b47a3e7d92f8d4f8489
Netanel <segal.netanel4@gmail.com>
2025-07-09 10:42:56 +0300

    added auth signup and login

--- models/user.model.js
+++ models/user.model.js
@@ -1,9 +1,25 @@
 import mongoose from "mongoose";
 
-const UserModel = mongoose.model("users", {
-  name: String,
-  email: String,
-  password: String,
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
 });
+
+const UserModel = mongoose.model("users", userSchema);
 
 export default UserModel;
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,23 +1,56 @@
+import bcrypt from "bcrypt";
 import { Router } from "express";
 import UserModel from "../models/user.model.js";
 
 const router = Router();
 
-router.post("/login", (req, res) => {
-  res.json({});
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    user.password = "********";
+    res.json(user);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.post("/signup", (req, res) => {
-  const { name, email, password } = req.body;
-  const user = new UserModel({ name, email, password });
-  user
-    .save()
-    .then(() => {
-      res.json(user);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
+router.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new UserModel({ name, email, password: hashedPassword });
+    await user.save();
+    user.password = "********";
+    res.status(201).json(user);
+  } catch (err) {
+    if (err.code === 11000) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    if (err.name === "ValidationError") {
+      return res.status(422).json({
+        message: err.message,
+      });
+    }
+
+    res.status(400).json(err);
+  }
 });
 
 export default router;
